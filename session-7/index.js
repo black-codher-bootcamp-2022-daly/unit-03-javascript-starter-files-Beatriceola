09eca01b32a3d787e21b065e99648fadeb26f117
Beatrice Olatunbosun <beatriceola95@hotmail.com>
2022-10-24 21:25:26 +0100

    add-session-2

--- session-7/index.js
+++ session-7/index.js
@@ -35,8 +35,17 @@ const people = [
   { name: 'killmonger', age: 32 },
 ];
 
+
 // This is a helper function that should be used in the final task of the session.
 // You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
 function compare(a, b) {
   return a.age - b.age;
 }
+
+const animals = ['cat', 'dog', 'wolf', 'lion', 'eagle', 'zebra'];
+
+animals.pop()
+console.log(animals)
+
+people.pop()
+console.log(people)
--- session-8/js/index.js
+++ session-8/js/index.js
@@ -1,3 +1,35 @@
 import displayPopulation from "./birmingham.js";
 import { fruits } from "./fruit.js";
 import createListOfFruits from "./giveMeFruits.js";
+
+let personA = {
+    name: 'Leena',
+    age: 21,
+    location: 'coventry',
+    likes: 'pizza',
+};
+
+let personB = {
+    name: 'Vanessa',
+    age: 21,
+    location: 'coventry',
+    likes: 'plantain'
+};
+
+let personC = {
+    name: 'Liz',
+    age: 31,
+    location: 'essex',
+    likes: 'gym' 
+};
+
+let personD = {
+    name: 'Rachael',
+    age: 26,
+    location: 'essex',
+    likes: 'books'
+};
+
+function biography(person) {
+    return
+}
