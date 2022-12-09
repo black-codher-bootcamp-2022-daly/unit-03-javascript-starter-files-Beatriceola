37c956245ed1dfc00e325a9c9590cedcbe6f5891
Beatrice Olatunbosun <beatriceola95@hotmail.com>
2022-12-09 09:05:46 +0000

    maths problems

--- session-4/index.js
+++ session-4/index.js
@@ -53,25 +53,28 @@ console.log(biography(personC));
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 6;
+const y = 4;
 
-// const addition = x + y;
+const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("Division: x + y " + division);
 
+const powerOf = x**y
+
+console.log("6 to the power of 4 is "  + powerOf);
 
 
 
